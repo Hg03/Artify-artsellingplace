84ef91c8e270b60645ebe6cddce7a3b7e55dd0cd
Harish Gehlot <69637720+Hg03@users.noreply.github.com>
2022-10-08 18:39:51 +0530

    Update Details.js

--- screens/Details.js
+++ screens/Details.js
@@ -1,92 +1,117 @@
 import React from "react";
-import { View, Text, SafeAreaView, Image, StatusBar, FlatList } from "react-native";
-
+import { View, Text, SafeAreaView, Image, StatusBar, FlatList, StyleSheet, Button, ScrollView, TouchableOpacity} from "react-native";
+import circle from '../assets/images/CirVec1.png';
+import { Avatar } from "react-native-paper";
+import AvtarImg from '../assets/images/person01.png';
 import { COLORS, SIZES, assets, SHADOWS, FONTS } from "../constants";
+import { Ionicons } from '@expo/vector-icons';
+import product from '../assets/images/shoes.jpg';
 import { CircleButton, RectButton, SubInfo, DetailsDesc, DetailsBid, FocusedStatusBar } from "../components";
+import Accordion from "../components/Accordion";
 
-const DetailsHeader = ({ data, navigation }) => (
-  <View style={{ width: "100%", height: 373 }}>
-    <Image
-      source={data.image}
-      resizeMode="cover"
-      style={{ width: "100%", height: "100%" }}
-    />
 
-    <CircleButton
-      imgUrl={assets.left}
-      handlePress={() => navigation.goBack()}
-      left={15}
-      top={StatusBar.currentHeight + 10}
-    />
+const Details = () => {
+  
+    return (
+    <SafeAreaView>
+    <ScrollView>
+    <View style = {styles.container}>
+      <Image source={circle} style = {{ width:200, height:150 }} />        
+      <View style = {{ alignItems: 'center', height:100, width:"100%", position:'absolute'}}>
+              
+        <Text style = {{ fontWeight:"bold" , fontSize : 30, lineHeight : 48, color: "#363488", top:70}}>
+          Product Details
+        </Text>
+        
+      </View>
+      <View style={styles.profileView}>
+        <Avatar.Image size={30} source={AvtarImg} style={{marginLeft:20}}  />
+   
+        <Text style={styles.ProfileName}>Harish Gehlot</Text>
 
-    <CircleButton
-      imgUrl={assets.heart}
-      right={15}
-      top={StatusBar.currentHeight + 10}
-    />
-  </View>
-);
-
-const Details = ({ route, navigation }) => {
-  const { data } = route.params;
-
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <FocusedStatusBar
-        barStyle="dark-content"
-        backgroundColor="transparent"
-        translucent={true}
-      />
-
-      <View
-        style={{
-          width: "100%",
-          position: "absolute",
-          bottom: 0,
-          paddingVertical: SIZES.font,
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "rgba(255,255,255,0.5)",
-          zIndex: 1,
-        }}
-      >
-        <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark} />
+        <TouchableOpacity>
+          <Ionicons name="call-outline" size={24} color="#363488" />
+        </TouchableOpacity>
+        
+        
       </View>
 
-      <FlatList
-        data={data.conts}
-        renderItem={({ item }) => <DetailsBid bid={item} />}
-        keyExtractor={(item) => item.id}
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{
-          paddingBottom: SIZES.extraLarge * 3,
-        }}
-        ListHeaderComponent={() => (
-          <React.Fragment>
-            <DetailsHeader data={data} navigation={navigation} />
-            <SubInfo />
-            <View style={{ padding: SIZES.font }}>
-              <DetailsDesc data={data} />
+      <View style = {{alignItems:'center',marginTop:'5%'}}>
+        <Image source={product} style = {styles.productImage}/>
+        <Text style = {styles.productTitle}>
+          Leather Shoes
+        </Text>
+        <Text style = {{fontSize:20,color:"#363488"}}>
+          800$
+        </Text>
+      </View>
+      
 
-              
-                <Text
-                  style={{
-                    fontSize: SIZES.font,
-                    fontFamily: FONTS.semiBold,
-                    color: COLORS.primary,
-                  }}
-                >
-                  Reach to me 
+      <View style = {{ width:"100%",height:40,marginTop:'2%'}} >
+        <Text style = {{ fontFamily:FONTS.bold, color:'#363488',marginTop:'3%',marginLeft:'2%' }}> Product Category</Text>
+        <Text style = {{ position :'absolute',marginLeft:'70%',marginTop:'3%',fontFamily:FONTS.bold,fontStyle:'italic'}}> Footwears</Text>
+      </View>
+      <Accordion title={'Description'} bodyText={'Description about shoesa;lksdfja;sdlfjdskfdjfkdfkdkdfkdfjkdfjkdfkjfkdjfkfkda'} />
 
-            
-                </Text>
-              
-            </View>
-          </React.Fragment>
-        )}
-      />
+      <TouchableOpacity style = {{ height:140}} >
+        <View style = {{ backgroundColor:'#363488',width:'80%',height:'40%',borderRadius:15,marginLeft :"10%",marginTop:"5%"}}>
+          <Text style = {{ color:'white',fontFamily:FONTS.bold,marginTop:'6%',textAlign:'center'}}> Proceed to pay</Text>
+        </View>
+      </TouchableOpacity>
+
+      
+
+      
+      
+
+      
+    </View>
+    </ScrollView>
     </SafeAreaView>
   );
 };
 
 export default Details;
+
+
+const styles=StyleSheet.create({
+
+  profileView:{
+        marginTop:"2%",
+        flexDirection:"row",
+        width:"90%",
+        position:"relative",
+        //marginLeft:"auto",
+        marginRight:"auto",
+        height:"auto",
+
+        
+    },
+    container : {
+      height : "100%",
+      backgroundColor : 'white',
+    },
+    ProfileName:{
+    fontWeight:"bold",
+    
+    fontSize:18,
+    textAlign:"center",
+    marginTop:"auto",
+    marginBottom:"auto",
+    marginLeft:10,
+    marginRight:"auto",
+    color:"#363488",
+    },
+    productImage : {
+      width:340,
+      height:280,
+      borderRadius:20
+    },
+    productTitle : {
+      fontSize:20,
+      marginTop:'5%',
+      fontStyle:'italic',
+      fontFamily:FONTS.bold,
+      color:'#363488'
+    },
+});
